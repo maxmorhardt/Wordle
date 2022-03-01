2e1f08dcc5c4d615bb6e197233cda1dce63606a5
Max Morhardt <maxmorhardt13@gmail.com>
2022-03-01 12:46:13 -0500

    Now can take user input with validation

--- src/Main.java
+++ src/Main.java
@@ -14,27 +14,31 @@ public class Main {
 	
 	// File name for the word list
 	private static final String WORD_LIST_NAME = "word_list.txt";
+	private static final int NUM_ATTEMPTS = 6;
 	
-	// Word list
+	// Words from the word list
 	private List<String> words;
 	
 	// Constructor
 	public Main() {
-		words = new ArrayList<>();
+		words = scanList(WORD_LIST_NAME);
 	}
 	
 	// Scans the word list and adds all words to a list
-	private void scanList(String fileName) {
+	private List<String> scanList(String fileName) {
+		List<String> list = new ArrayList<>();
 		try {
 			Scanner in = new Scanner(new File(fileName));
 			while (in.hasNext()) {
-				words.add(in.next());
+				list.add(in.next());
 			}
 			in.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("The file was not found");
 			e.printStackTrace();
 		}
+		assert list != null;
+		return list;
 	}
 	
 	// Gets a random number within a range
@@ -43,29 +47,39 @@ public class Main {
 	}
 	
 	// Gets a random word from a word list
-	private String getRandomWord(List<String> words) {
+	private String getRandomWord() {
 		int index = getRandomNumber(0, words.size()-1);
 		return words.get(index);
 	}
 	
 	// Allows user to make a guess
 	private String makeGuess() {
-		String guess = "";
-		do {
-			Scanner in = new Scanner(System.in);
-			guess = in.next();
-		} while (!words.contains(guess));
+		String guess;
+		Scanner in = new Scanner(System.in);
+		System.out.print ("Enter a guess: ");
+		guess = in.next();
+		while (!words.contains(guess)) {
+	        System.out.println("Not in the word list");
+	        guess = in.next();
+        }
 		return guess;
 	}
 	
 	// Runs the program
 	private void runProgram() {
-		// TODO
-		return;
+		String wordToGuess = getRandomWord();
+		int numAttempts = NUM_ATTEMPTS;
+		while (numAttempts > 0) {
+			System.out.println("Number of attempts remaining: " + numAttempts);
+			makeGuess();
+			numAttempts--;
+			System.out.println();
+		}
 	}
 	
 	// Main method
 	public static void main(String[] args) {
+		new Main().runProgram();
 	}
 	
 }
