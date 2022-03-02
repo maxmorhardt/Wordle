1b20d469cc5f20bf58102e3675b48adfad06f788
Max Morhardt <maxmorhardt13@gmail.com>
2022-03-01 19:15:54 -0500

    Added boxes to the view

--- src/application/WordleView.java
+++ src/application/WordleView.java
@@ -5,10 +5,8 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -20,25 +18,32 @@ public class WordleView extends Application {
 	// Game scene attributes
 	private static final int FRAMES_PER_SECOND = 5;
 	private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
-	private static final int EXTRA_VERTICAL = 175;
-	private static final int EXTRA_HORIZONTAL = 375;
-	private static final int CELL_SIZE = 20;
-	private static final int SPACING = 10;
+	private static final int SCENE_WIDTH = 900;
+	private static final int SCENE_HEIGHT = 850;
+	private static final int CELL_SIZE = 50;
+	private static final int CELL_SPACING = 20;
+	private static final int ROOT_SPACING = 10;
 	private static final int WORD_LENGTH = 5;
 	private static final int NUM_GUESSES = 6; 
 
 	// Scene variables
 	private Scene myScene;
-	private VBox root;
 
 	// Grid variable
 	private Rectangle[][] grid;
 
 	// Controller variable
 	private WordleController controller;
+	
+	public WordleView() {
+		grid = new Rectangle[WORD_LENGTH][NUM_GUESSES];
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		// Sets the title of the application
+		primaryStage.setTitle("Wordle");
+		
 		// Scene
 		myScene = setupScene();
 
@@ -59,17 +64,18 @@ public class WordleView extends Application {
 	 * 
 	 * @return
 	 */
-	protected Scene setupScene() {
+	private Scene setupScene() {
+		// Sets up the boxes for text and color to be displayed
+		GridPane boxes = setupBoxes();
+		
 		// Sets up root to align all elements
-		root = new VBox();
+		VBox root = new VBox();
 		root.setAlignment(Pos.CENTER);
-		root.setSpacing(SPACING);
-		root.setPadding(new Insets(SPACING, SPACING, SPACING, SPACING));
-		root.getChildren().addAll();
+		root.setPadding(new Insets(ROOT_SPACING, ROOT_SPACING, ROOT_SPACING, ROOT_SPACING));
+		root.getChildren().addAll(boxes);
 
 		// Creates scene
-		Scene scene = new Scene(root, WORD_LENGTH * CELL_SIZE + EXTRA_HORIZONTAL, NUM_GUESSES * CELL_SIZE + EXTRA_VERTICAL,
-				Color.ANTIQUEWHITE);
+		Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT, Color.WHITE);
 		return scene;
 	}
 
@@ -78,16 +84,20 @@ public class WordleView extends Application {
 	 * 
 	 * @return
 	 */
-	protected Group setupGrid() {
-		Group gridDrawing = new Group();
+	private GridPane setupBoxes() {
+		GridPane boxes = new GridPane();
 		for (int i = 0; i < grid.length; i++) {
 			for (int j = 0; j < grid[i].length; j++) {
 				Rectangle currCell = new Rectangle(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+				currCell.setFill(Color.GRAY);
+				boxes.add(currCell, i, j);
 				grid[i][j] = currCell;
-				gridDrawing.getChildren().add(currCell);
 			}
 		}
-		return gridDrawing;
+		boxes.setAlignment(Pos.TOP_CENTER);
+		boxes.setHgap(CELL_SPACING);
+		boxes.setVgap(CELL_SPACING);
+		return boxes;
 	}
 	
 	/**
