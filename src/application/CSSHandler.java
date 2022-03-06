cc2da308062b73f80d6a14ea64ffc8aa0433419f
Max Morhardt <maxmorhardt13@gmail.com>
2022-03-06 12:06:22 -0500

    Added classes for CSS and keyboard handling

---
+++ src/application/CSSHandler.java
@@ -0,0 +1,10 @@
+package application;
+
+/**
+ * Class for hardcoding CSS since my JavaFX doesnt let me use stylesheets
+ * 
+ * @author Max Morhardt
+ */
+public class CSSHandler {
+
+}
---
+++ src/application/KeyboardHandler.java
@@ -0,0 +1,10 @@
+package application;
+
+/**
+ * Handles both normal and GUI keyboard inputs
+ * 
+ * @author Max Morhardt
+ */
+public class KeyboardHandler {
+
+}
